859fbcb6ffaee71649648f48fe0c6ddac3e592ec
gaoshan <mrgaoshan@foxmail.com>
2017-07-11 10:21:15 +0800

    新增钉钉接入

--- src/main/java/com/wormwood/controller/UserController.java
+++ src/main/java/com/wormwood/controller/UserController.java
@@ -6,14 +6,16 @@ import com.wormwood.result.ResultEnum;
 import com.wormwood.vo.user.UserListVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by kasimodo on 2017-07-10.
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController extends DDBaseController {
 
@@ -21,8 +23,17 @@ public class UserController extends DDBaseController {
     private DingDingClient dingDingClient;
 
     @RequestMapping(value = "/findUserList", method = RequestMethod.GET)
-    public UserListVO findUserList(String token) {
-        UserListVO userList = dingDingClient.findUserList(getToken().getAccess_token(), "1");
-        return userList;
+    @ResponseBody
+    public Response<UserListVO> findUserList(String token) {
+        UserListVO userListVo = dingDingClient.findUserList(getToken().getAccess_token(), "1");
+        if (userListVo.getErrcode() != 0) {
+            return new Response<>(userListVo, ResultEnum.FAIL);
+        }
+        return new Response<>(userListVo, ResultEnum.SUCCESS);
+    }
+
+    @RequestMapping("/toStaffList")
+    public String toStaffList() {
+        return "user/staffList";
     }
 }
